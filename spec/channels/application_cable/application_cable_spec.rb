c32b02834dd7eaaf4703a47945d100587694279e
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-13 11:20:39 +0100

    add empty tests

---
+++ spec/channels/application_cable/application_cable_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationCable, type: :cable do
+  it 'works' do
+  end
+end
---
+++ spec/jobs/application_job_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationJob, type: :job do
+  it 'works' do
+  end
+end
