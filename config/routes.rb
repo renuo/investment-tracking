09c86f2e68389b485c6c49dbf11053d921c62236
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-13 11:24:01 +0100

    add time entries to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'home/check'
   get 'home/index'
 
+  get 'time_entries/index'
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
