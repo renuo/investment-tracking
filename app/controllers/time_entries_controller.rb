0a794ce3fb3a2cf41f0a4ecffb707965e1a8d276
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-14 15:22:11 +0100

    change simulation to controller

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,7 +1,13 @@
 class TimeEntriesController < ApplicationController
   def index
-    @count_times = RedmineTimeEntriesAggregator.new.count_entries
-    @name_and_id = RedmineTimeEntriesAggregator.new.id_and_user
-    @aggregated_values = RedmineTimeEntriesAggregator.new.all_investment_time_per_user
+    csv_worked_hours = RedmineRequest.new.retrieve_time_entries
+    csv_investment_time = RedmineRequest.new.retrieve_investment_time
+
+    parsed_worked_hours = CsvParser.new.parse_to_hash(csv_worked_hours)
+    parsed_investment_time = CsvParser.new.parse_to_hash(csv_investment_time)
+
+    @aggregated_datas = RedmineTimeEntriesAggregator.new
+                                                    .generate_aggregated_data(parsed_worked_hours,
+                                                                              parsed_investment_time)
   end
 end
