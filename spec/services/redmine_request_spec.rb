60c631339dc2aa55ceb96d222c3f8a2cbcf7996a
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-14 15:19:24 +0100

    Add spec tests

--- spec/services/redmine_request_spec.rb
+++ spec/services/redmine_request_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe RedmineTimeEntriesAggregator, type: :service do
+RSpec.describe RedmineRequest, type: :service do
   subject { described_class.new }
 
   before(:each) do
-    @mock_aggregator = RedmineTimeEntriesAggregator.new
+    @mock_aggregator = RedmineRequest.new
     @mock_data = [{ 'user' => { 'id' => 1, 'name' => 'Max Muster' }, 'hours' => 0.5 },
                   { 'user' => { 'id' => 1, 'name' => 'Max Muster' }, 'hours' => 1 }]
   end
