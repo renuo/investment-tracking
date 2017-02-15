1056204b6dc9848c3704bb03bc74b43039048f96
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-15 17:48:37 +0100

    add web mock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
+require 'webmock/rspec'
 
 Capybara.javascript_driver = :webkit
 
