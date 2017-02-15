542fd4e4c70570ecdf7546255513cd69ef015c87
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-15 17:48:18 +0100

    change redmine aggregator

--- app/services/redmine_time_entries_aggregator.rb
+++ app/services/redmine_time_entries_aggregator.rb
@@ -1,56 +1,86 @@
 class RedmineTimeEntriesAggregator
-  def generate_aggregated_data(csv_time_entries, csv_investment_time)
-    calculated_time_entries = calculate_investment_time_user(csv_time_entries)
-    add_difference = difference_taken_and_open(calculated_time_entries, csv_investment_time)
-    add_no_investment_time_user(add_difference)
+  def initialize(total_time_entries, investment_time_entries)
+    @hashes_total_times = total_time_entries
+    @hashes_investment_times = investment_time_entries
+    @add_investment_time_to_hashes = nil
+    @renamed_hashes_total_times = nil
+    @renamed_hashes_investment_time = nil
+    @merged_hashes = nil
+    @added_open_investment_time = nil
+    @added_proportion_open_worked = nil
+    @added_proportion_used_open = nil
+    @sort_proportion_open_worked = nil
+  end
+
+  def aggregate_time_entries
+    add_total_of_investment_time
+    rename_hashes_time_entries
+    rename_hashes_investment_time
+    merge_both_hashed
+    calculate_open_investment_time
+    add_proportion_open_to_worked
+    add_used_open_proportion
+    sort_by_proportion
   end
 
   private
 
-  def calculate_investment_time_user(csv_datas)
-    csv_datas.each do |data|
-      data['Investment_time_sum'] = data['Gesamtzeit'].to_f / 5
+  def add_total_of_investment_time
+    @add_investment_time_to_hashes = @hashes_total_times.each do |hash|
+      hash['investment_time_total'] = (hash['Gesamtzeit'].to_f / 5).round(2)
     end
   end
 
-  def difference_taken_and_open(csv_time_datas, csv_investment_datas)
-    # [{
-    #   name: 'Alicia',
-    #   worked_hours: 120,
-    #   worked_investment_hours: 8
-    # }]
-    #
-    # worked_hours = csv_time_datas.map do |row|
-    #   { name: row['Benutzer'], worked_hours: row['Gesamtzeit'] }
-    # end
-    #
-    # worked_investment_hours = csv_time_datas.map do |row|
-    #   { name: row['Benutzer'], worked_investment_hours: row['Gesamtzeit'] }
-    # end
-    #
-    # (worked_hours + worked_investment_hours).group_by do |hour_person_pair|
-    #   hour_person_pair[:name]
-    # end.map{|k,v| v.reduce(:merge)}
-    #
-    # users = []
+  def rename_hashes_time_entries
+    @renamed_hashes_total_times = @add_investment_time_to_hashes.each do |hash|
+      hash['name'] = hash.delete('Benutzer')
+      hash['worked_hours'] = hash.delete('Gesamtzeit')
+    end
+  end
 
-    csv_time_datas.each do |all|
-      csv_investment_datas.each do |taken|
-        taken_investment_time = taken['Gesamtzeit']
-        if all['Benutzer'] == taken['Benutzer']
-          all['Investment_time_used'] = taken_investment_time
-          all['Difference_investment_time'] = (all['Investment_time_sum'].to_f - taken_investment_time.to_f).round(2)
+  def rename_hashes_investment_time
+    @renamed_hashes_investment_time = @hashes_investment_times.each do |hash|
+      hash['name'] = hash.delete('Benutzer')
+      hash['worked_investment_hours'] = hash.delete('Gesamtzeit')
+    end
+  end
+
+  def merge_both_hashed
+    @merged_hashes = @renamed_hashes_total_times.each do |total_hours|
+      @renamed_hashes_investment_time.each do |used_hours|
+        if total_hours['name'] == used_hours['name']
+          total_hours.merge!(used_hours)
         end
       end
     end
   end
 
-  def add_no_investment_time_user(difference_data)
-    difference_data.each do |data|
-      unless data.key?('Investment_time_used')
-        data['Investment_time_used'] = 0
-        data['Difference_investment_time'] = data['Investment_time_sum']
+  def calculate_open_investment_time
+    @added_open_investment_time = @merged_hashes.each do |hash|
+      if hash.key?('worked_investment_hours')
+        hash['open_investment_time'] = (hash['investment_time_total'].to_f - hash['worked_investment_hours'].to_f)
+                                       .round(2)
+      else
+        hash['worked_investment_hours'] = 0
+        hash['open_investment_time'] = hash['investment_time_total'].to_f.round(2)
       end
     end
+  end
+
+  def add_proportion_open_to_worked
+    @added_proportion_open_worked = @added_open_investment_time.each do |hash|
+      hash['percent_used_to_worked'] = (hash['worked_investment_hours'].to_f / hash['worked_hours'].to_f * 100).round(2)
+    end
+  end
+
+  def add_used_open_proportion
+    @added_proportion_used_open = @added_proportion_open_worked.each do |hash|
+      hash['percent_open_to_used'] = (hash['open_investment_time'].to_f / hash['investment_time_total'].to_f * 100)
+                                     .round(2)
+    end
+  end
+
+  def sort_by_proportion
+    @sort_proportion_open_worked = @added_proportion_used_open.sort_by { |k| k['percent_used_to_worked'] }
   end
 end
