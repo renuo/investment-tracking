7cc4ceb007d72201fd665c78136cd7cc97268ac6
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-15 17:48:59 +0100

    change time entries controller

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,13 +1,13 @@
 class TimeEntriesController < ApplicationController
   def index
-    csv_worked_hours = RedmineRequest.new.retrieve_time_entries
-    csv_investment_time = RedmineRequest.new.retrieve_investment_time
+    csv_worked_hours = RedmineRequest.new('/time_entries/report.csv?').request_redmine_for_entries
+    @csv_investment_time = RedmineRequest.new('/projects/renuo-investments/time_entries/report.csv?')
+                                         .request_redmine_for_entries
 
-    parsed_worked_hours = CsvParser.new.parse_to_hash(csv_worked_hours)
-    parsed_investment_time = CsvParser.new.parse_to_hash(csv_investment_time)
+    parsed_worked_hours = CsvParser.new(csv_worked_hours).parse_to_hash
+    parsed_investment_time = CsvParser.new(@csv_investment_time).parse_to_hash
 
-    @aggregated_datas = RedmineTimeEntriesAggregator.new
-                                                    .generate_aggregated_data(parsed_worked_hours,
-                                                                              parsed_investment_time)
+    @aggregated_time_entries = RedmineTimeEntriesAggregator.new(parsed_worked_hours, parsed_investment_time)
+                                                           .aggregate_time_entries
   end
 end
