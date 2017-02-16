1364a6439a2b0d15a29661e865f2526e02dddd9e
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-16 16:48:12 +0100

    adjust time entries controller to work with new services

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,13 +1,12 @@
 class TimeEntriesController < ApplicationController
   def index
-    csv_worked_hours = RedmineRequest.new('/time_entries/report.csv?').request_redmine_for_entries
-    @csv_investment_time = RedmineRequest.new('/projects/renuo-investments/time_entries/report.csv?')
-                                         .request_redmine_for_entries
+    csv_it_hours = RedmineRequestIt.new.request_redmine_for_entries
+    csv_total_hours = RedmineRequest.new.request_redmine_for_entries
 
-    parsed_worked_hours = CsvParser.new(csv_worked_hours).parse_to_hash
-    parsed_investment_time = CsvParser.new(@csv_investment_time).parse_to_hash
+    parsed_it_hours = CsvParser.new(csv_it_hours).parse_to_hash
+    parsed_total_hours = CsvParser.new(csv_total_hours).parse_to_hash
 
-    @aggregated_time_entries = RedmineTimeEntriesAggregator.new(parsed_worked_hours, parsed_investment_time)
+    @aggregated_time_entries = RedmineTimeEntriesAggregator.new(parsed_total_hours, parsed_it_hours)
                                                            .aggregate_time_entries
   end
 end
