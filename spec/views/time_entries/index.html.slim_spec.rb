b6f2d9a090e597383c7ca437ef54b59daf04020a
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-16 16:48:29 +0100

    adjust test to new services

--- spec/views/time_entries/index.html.slim_spec.rb
+++ spec/views/time_entries/index.html.slim_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'time_entries/index.html.slim', type: :view do
   describe 'time_entries' do
     it 'it shows the data of a user' do
-      assign(:aggregated_time_entries, [{ 'name' => 'Max Muster' }])
+      assign(:redmine_request_it_griffin, [{'name' => 'Max Muster' }])
 
       render
 
