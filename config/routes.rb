28bd94b79ac85cafdf7a4e754bd86a9f84d8ae2a
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-28 15:30:34 +0100

    redefine time_entries#index to first page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get 'time_entries/index'
 
-  root 'home#index'
+  root 'time_entries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
