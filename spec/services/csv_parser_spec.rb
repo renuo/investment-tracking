43ebc4929b74613415874e550259982f661f5b5f
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-02-28 15:25:42 +0100

    write test for css parser

--- spec/services/csv_parser_spec.rb
+++ spec/services/csv_parser_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CsvParser, type: :service do
-  subject { described_class.new(url_report_path) }
+  subject do
+    described_class.new(double(:response, body:
+    "User,2017-1,2017-2,Total time\nEmily Wangler,\"\",7.26,7.26\nPascal Andermatt,0.85,24.24,25.09\n"))
+  end
 
   describe 'request redmine for entries' do
     it 'makes a http request' do
+      expected_solution = [{ 'User' => 'Emily Wangler', '2017-1' => '', '2017-2' => '7.26', 'Total time' => '7.26' },
+                           { 'User' => 'Pascal Andermatt', '2017-1' => '0.85', '2017-2' => '24.24',
+                             'Total time' => '25.09' }]
+      expect(subject.parse_to_hash).to eq(expected_solution)
     end
   end
 end
