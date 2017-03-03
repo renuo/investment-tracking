cf77f25041f6c2b4947f987467bf75966a4af6af
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-03 18:31:43 +0100

    create schema for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170303104024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "times_per_users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "ticketId"
+    t.integer  "workedTimes"
+    t.integer  "surplus"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
