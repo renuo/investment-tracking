142576551fba8d58e56a19a6aec649dcb2dedcdc
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-28 13:48:22 +0200

    Delete fetching json from Redline

--- db/migrate/20170303103559_times_per_user.rb
+++
@@ -1,4 +0,0 @@
-class TimesPerUser < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- lib/tasks/fetch_tickets_from_redmine.rb
+++
@@ -1,7 +0,0 @@
-namespace :fetch_redmine_data do
-  desc 'fetches every hour the data from Redmine'
-
-  task mail_export: :environment do
-    FetchJsonFromRedmine
-  end
-end
