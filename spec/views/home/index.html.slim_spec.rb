03b62aef67f45ad5d0f71ad6c831e66075032ac3
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-28 17:31:25 +0200

    Adjust the view of home because of the other changes in the home controller and the calculations

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe 'home/index.html.slim', type: :view do
   describe 'time_entries' do
     it 'it shows the data of a user' do
-      assign(:times_per_users, [{ 'name' => 'Max Muster', 'open_investment_time' => 1,
-                                  'percent_used_to_worked' => 2, 'percent_used_to_open' => 2 }])
+      assign(:sorted_entries, [{ 'name' => 'Max Muster', 'open_investment_time' => 1 }])
 
       render
 
