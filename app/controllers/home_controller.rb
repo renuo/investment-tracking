8cc83e5df94c02e6ade20940dc70bff652a41e83
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-29 12:16:45 +0200

    Call RedmineRequestJson in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    RedmineRequestJson.new.fetch_json_from_redmine
+  end
 
   def check
     val = ActiveRecord::Base.connection.execute('select 1+2 as val').first['val']
