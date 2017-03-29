d3a4d22760d05ed9f125cc939d45d3f7a2cae565
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-29 17:38:27 +0200

    Take out logic of view and put it into a helper

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe HomeHelper, type: :helper do
+  describe 'progress_bar_status' do
+    it 'should return the progress-bar-danger if the quantity is more or equal than 60' do
+      open_investment_time_in_hours = { 'open_investment_time' => 60 }
+      expect(helper.progress_bar_status(open_investment_time_in_hours)).to eq('progress-bar-danger')
+    end
+    it 'should return the progress-bar-warning if the quantity is less than 60 and more than 40' do
+      open_investment_time_in_hours = { 'open_investment_time' => 59 }
+      expect(helper.progress_bar_status(open_investment_time_in_hours)).to eq('progress-bar-warning')
+    end
+    it 'should return the progress-bar-warning if the quantity is less than 60 and more than 40' do
+      open_investment_time_in_hours = { 'open_investment_time' => 40 }
+      expect(helper.progress_bar_status(open_investment_time_in_hours)).to eq('progress-bar-warning')
+    end
+    it 'should return the progress-bar-success if the quantity is less 40' do
+      open_investment_time_in_hours = { 'open_investment_time' => 39 }
+      expect(helper.progress_bar_status(open_investment_time_in_hours)).to eq('progress-bar-success')
+    end
+  end
 end
+
+# def progress_bar_status(entries)
+#   open_investment_time_in_hours = entries['open_investment_time']
+#   if open_investment_time_in_hours >= 60
+#     'progress-bar-danger'
+#   elsif open_investment_time_in_hours >= 40 && open_investment_time_in_hours < 60
+#     'progress-bar-warning'
+#   else
+#     'progress-bar-success'
+#   end
+# end
