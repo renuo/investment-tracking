f2757f4cfdad5d284285c7199ea6ad8d9973c354
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-29 09:10:28 +0200

    Rename sort_time service and its variables

---
+++ spec/services/sort_by_proportion_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe SortTime, type: :service do
+  let(:aggregated_time) do
+    [{ 'name' => 'Hans Meier', 'proportion_used_to_worked' => 0.2 },
+     { 'name' => 'Max Muster', 'proportion_used_to_worked' => 0.1 }]
+  end
+
+  describe 'request redmine for entries' do
+    it 'makes a http request' do
+      expected_solution = [{ 'name' => 'Max Muster', 'proportion_used_to_worked' => 0.1 },
+                           { 'name' => 'Hans Meier', 'proportion_used_to_worked' => 0.2 }]
+      expect(described_class.new(aggregated_time).sort_by_proportion).to eq(expected_solution)
+    end
+  end
+end
