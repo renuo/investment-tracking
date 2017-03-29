8cf280251d34ebd4cd2776eed9af86eba4c50b5e
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-29 15:19:27 +0200

    Adjust home_controller and make new private methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,20 @@ class HomeController < ApplicationController
   end
 
   def index
+    @final_entries = data_service.alter_csv_to_final_data
   end
 
   private
 
+  def data_service
+    AlterCsvData.new(csv_investment_entries, csv_total_entries)
   end
 
+  def csv_investment_entries
+    RedmineRequestCsvIt.new.request_redmine_for_entries
   end
 
+  def csv_total_entries
+    RedmineRequestCsv.new.request_redmine_for_entries
   end
 end
