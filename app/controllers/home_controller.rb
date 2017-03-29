f76a5469b25a9e458d118b74822380b020ece6d0
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-29 12:30:30 +0200

    Delete constructor of home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,4 @@
 class HomeController < ApplicationController
-  def initialize
-    @csv_investment_entries = nil
-    @csv_total_entries = nil
-
-    @hashes_investment_entries = nil
-    @hashes_total_entries = nil
-
-    @concatenated_entries = nil
-
-    @entries_with_calculations = nil
-
-    @leftover_of_deletion_entries = nil
-
-    @sorted_entries = nil
-    super
-  end
-
   def check
     val = ActiveRecord::Base.connection.execute('select 1+2 as val').first['val']
     render plain: "1+2=#{val}"
