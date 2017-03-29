1587481a3a483c8db410c3058dc03a003ccce835
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-29 15:21:08 +0200

    Adjust test of index html

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'home/index.html.slim', type: :view do
   describe 'time_entries' do
     it 'it shows the data of a user' do
-      assign(:sorted_entries, [{ 'name' => 'Max Muster', 'open_investment_time' => 1 }])
+      assign(:final_entries, [{ 'name' => 'Max Muster', 'open_investment_time' => 1 }])
 
       render
 
