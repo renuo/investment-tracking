f4ef8414ca900c6737c7fbd6c9c3a04a7d80bf34
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-03-31 08:51:54 +0200

    Adjust the controller to get the information of the json readmine requests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    RedmineRequestJson.new.fetch_json_from_redmine
+    new_time_entries = RedmineRequestJson.new.fetch_json_from_redmine
+    investment_time_entries = RedmineRequestJsonInvestmentTime.new.fetch_json_from_redmine
+    @concatenated_hashes = ConcatenateHashes.new(new_time_entries, investment_time_entries).concatenate
+    @all_users = User.all
   end
 
   def check
