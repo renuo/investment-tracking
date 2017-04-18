585703ef80deb81db58909f5feb5d3e0b9c7042f
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-18 09:51:23 +0200

    Get Json from Readmine

---
+++ app/services/redmine_issue.rb
@@ -0,0 +1,42 @@
+class RedmineIssue
+  def initialize
+    @offset = 0
+    @date_of_latest_import = Import.last['latest_import']
+    @new_entries_left = true
+    @json_from_redmine = nil
+    @new_time_entries = []
+  end
+
+  def entries_since_latest_import
+    while @new_entries_left
+      fetch_json_with_new_entries
+      sort_by_created_on
+      extract_new_entries_from_json
+      @offset += 1
+      entries_since_latest_import
+    end
+
+    @new_time_entries
+  end
+
+  private
+
+  def fetch_json_with_new_entries
+    @json_from_redmine = RedmineRequestJson.new(@offset).fetch_json_from_redmine
+  end
+
+  def sort_by_created_on
+    @json_from_redmine = @json_from_redmine.sort_by { |key| key['created_on'] }.reverse
+  end
+
+  def extract_new_entries_from_json
+    @json_from_redmine.each do |entry|
+      if entry['created_on'] > @date_of_latest_import
+        @new_time_entries << entry
+      else
+        @new_entries_left = false
+        break
+      end
+    end
+  end
+end
