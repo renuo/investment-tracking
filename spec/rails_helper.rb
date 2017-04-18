b39cd4f56f3795a520e9b682313a84db61725745
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-18 10:12:35 +0200

    Add database cleaner to spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,12 +5,33 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 require 'webmock/rspec'
+require 'database_cleaner'
 
 Capybara.javascript_driver = :webkit
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.include FactoryGirl::Syntax::Methods
 
   config.infer_spec_type_from_file_location!
