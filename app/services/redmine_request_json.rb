3ae4dfae1f6a609d5a4c94b71e7c3201f7c2d496
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-18 09:53:03 +0200

    Make readmine request

--- app/services/redmine_request_json.rb
+++ app/services/redmine_request_json.rb
@@ -1,61 +1,28 @@
 class RedmineRequestJson
-  def initialize
-    @latest_import_reached = false
-
-    @offset = 0
-
+  def initialize(offset)
+    @offset = offset
+    @res = nil
     @response_body = nil
-
-    @new_time_entries = []
-
-    @newest = nil
-
-    @date_of_latest_import = Import.last['latest_import']
-
-    @data_for_database = nil
   end
 
   def fetch_json_from_redmine
-    if !@latest_import_reached
-      http_request(@offset)
-      add_new_entries
-      @offset += 1
-      fetch_json_from_redmine
-    end
-
-    @date_of_latest_import  = @response_body.first["created_on"]
-
-    calculate_entries_for_db
-    @newest
+    http_request
+    extract_request_time_entries
+    @response_body
   end
 
-  def http_request(offset)
+  private
+
+  def http_request
     uri = URI('https://redmine.renuo.ch/time_entries.json?')
-    params = { limit: '100', offset: (offset * 100).to_s, key: ENV['REDMINE_API_KEY'] }
+    params = { limit: '100', offset: (@offset * 100).to_s, key: ENV['REDMINE_API_KEY'] }
     uri.query = URI.encode_www_form(params)
 
-    res = Net::HTTP.get_response(uri)
-    body = res.body
+    @res = Net::HTTP.get_response(uri)
+  end
 
+  def extract_request_time_entries
+    body = @res.body
     @response_body = JSON.parse(body)['time_entries']
-  end
-
-  def add_new_entries
-    @response_body.each do |entry|
-      if entry['created_on'] > @date_of_latest_import
-        @new_time_entries << {"user_id" => entry["user"]["id"], "name" => entry["user"]["name"], "hours" => entry["hours"]}
-      else
-        @latest_import_reached = true
-        break
-      end
-    end
-  end
-
-  def calculate_entries_for_db
-    @newest = @new_time_entries.group_by { |hash| hash["user_id"]}.map do |id, hashes|
-      hashes.reduce do |hash, next_hash|
-        hash.merge(next_hash) { |key, v1, v2| v1 == v2 ? v1 : (v1 + v2)}
-      end
-    end
   end
 end
