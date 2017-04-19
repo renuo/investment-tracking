e184ef612dd537eb384404e215938167d9fb13b4
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-19 19:25:50 +0200

    change validation to less or qual to

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
   validates :name, presence: true
   validates :redmine_user_id, presence: true
-  validates :open_investment_time, numericality: { less_than: 81 }
+  validates :open_investment_time, numericality: { less_than_or_equal_to: 80 }
 end
