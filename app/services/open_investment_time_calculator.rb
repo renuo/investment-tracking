c501145bd49accc54c8fb3b9acebdc6d9670a4de
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-19 18:05:36 +0200

    Make a service which calculates the new times to the old investment time

---
+++ app/services/open_investment_time_calculator.rb
@@ -0,0 +1,68 @@
+class OpenInvestmentTimeCalculator
+  def initialize(time_entries)
+    @newest_time_entries = time_entries
+    @aggregated_times_per_employee = nil
+
+    @employee_credentials = nil
+    @employee_id = nil
+    @employee_time_entries = nil
+    @employee = nil
+    @employee_investment_time = nil
+
+    @ratio_of_investment_time = 4
+  end
+
+  def add_time_entries_to_employees
+    @aggregated_times_per_employee = @newest_time_entries.map do |time_entries_per_employee|
+      extract_information(time_entries_per_employee)
+      add_entries_to_investment_time
+      set_new_investment_time
+      @employee
+    end
+  end
+
+  private
+
+  def extract_information(employee_entries)
+    @employee_credentials = employee_entries[0]
+    @employee_id = @employee_credentials[:redmine_user_id]
+    @employee_time_entries = employee_entries[1]
+    @employee = Employee.find_by(redmine_user_id: @employee_id)
+    @employee_investment_time = @employee.open_investment_time
+  end
+
+  def add_entries_to_investment_time
+    @employee_time_entries.each do |time_entry|
+      if entry_is_investment_time(time_entry)
+        subtract_investment_time(time_entry['hours'])
+      else
+        add_investment_time(time_entry['hours'])
+        @employee_investment_time = 80 if @employee_investment_time > 80
+      end
+    end
+  end
+
+  def entry_is_investment_time(time_entry)
+    renuo_investments = 138
+    investments_elf = 129
+    redmine_communicator = 141
+    redmine_estimator = 130
+    investments_griffin = 139
+    investments_inters = 140
+
+    [renuo_investments, investments_elf, redmine_communicator, redmine_estimator,
+     investments_griffin, investments_inters].include? time_entry['project']['id']
+  end
+
+  def subtract_investment_time(investment_hours)
+    @employee_investment_time -= investment_hours
+  end
+
+  def add_investment_time(worked_hours)
+    @employee_investment_time += worked_hours / @ratio_of_investment_time
+  end
+
+  def set_new_investment_time
+    @employee.open_investment_time = @employee_investment_time
+  end
+end
---
+++ spec/services/open_investment_time_calculator_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe OpenInvestmentTimeCalculator, type: :service do
+  describe 'add time entries to employees' do
+    before(:each) do
+      Employee.create(redmine_user_id: 1, name: 'Max Muster', open_investment_time: 2)
+      Employee.create(redmine_user_id: 2, name: 'Hans Meier', open_investment_time: 79)
+    end
+
+    context 'the open investment time is not over 80h' do
+      let(:time_entries) do
+        { { redmine_user_id: 1, name: 'Max Muster' } => [{ 'user' => { 'name' => 'Max Muster', 'id' => 1 },
+                                                           'project' => { 'id' => 1 }, 'hours' => 10 }],
+          { redmine_user_id: 2, name: 'Hans Meier' } => [{ 'user' => { 'name' => 'Hans Meier', 'id' => 2 },
+                                                           'project' => { 'id' => 129 }, 'hours' => 2 },
+                                                         { 'user' => { 'name' => 'Hans Meier', 'id' => 2 },
+                                                           'project' => { 'id' => 140 }, 'hours' => 2 }] }
+      end
+
+      it 'adds the time entries to the existing investment time of the db' do
+        updated_employees = described_class.new(time_entries).add_time_entries_to_employees
+        expect(updated_employees[0].open_investment_time).to be(4.0)
+        expect(updated_employees[1].open_investment_time).to be(75.0)
+      end
+    end
+
+    context 'the open investment time is over 80h' do
+      let(:time_entries) do
+        { { redmine_user_id: 1, name: 'Max Muster' } => [{ 'user' => { 'name' => 'Max Muster', 'id' => 1 },
+                                                           'project' => { 'id' => 1 }, 'hours' => 10 }],
+          { redmine_user_id: 2, name: 'Hans Meier' } => [{ 'user' => { 'name' => 'Hans Meier', 'id' => 2 },
+                                                           'project' => { 'id' => 1 }, 'hours' => 10 }] }
+      end
+
+      it 'adds the time entries to the existing investment time of the db' do
+        updated_employees = described_class.new(time_entries).add_time_entries_to_employees
+        expect(updated_employees[0].open_investment_time).to be(4.0)
+        expect(updated_employees[1].open_investment_time).to be(80.0)
+      end
+    end
+  end
+end
