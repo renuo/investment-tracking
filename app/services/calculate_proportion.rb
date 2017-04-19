7e504881fce55fabfbf255dabae408e95ac62a05
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-19 19:37:10 +0200

    Take out unused code

--- app/services/calculate_proportion.rb
+++ app/services/calculate_proportion.rb
@@ -2,7 +2,6 @@ class CalculateProportion
   def initialize(entries)
     @time_entries = entries
     @proportion_of_investment_time = 4.0
-    @limit_of_investment_time = 80
   end
 
   def calculate
