3f7c485e50abc07b28a19df30c122b22fc95c9e4
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-19 18:06:47 +0200

    Make a service which aggregates the new time entries and adds it to the DB in the end

---
+++ app/services/add_newest_time_entries.rb
@@ -0,0 +1,50 @@
+class AddNewestTimeEntries
+  def initialize(time_entries)
+    @new_time_entries = time_entries
+    @grouped_time_entries = nil
+    @employees = nil
+  end
+
+  def add_to_db
+    sort_and_group_entries
+    ensure_all_employee_are_in_db
+    add_time_entries_to_employees
+    save_information_to_db
+  end
+
+  private
+
+  def sort_and_group_entries
+    @grouped_time_entries = TimeEntriesPreparator.new(@new_time_entries).sort_and_group
+  end
+
+  def ensure_all_employee_are_in_db
+    @grouped_time_entries.map do |time_entries_per_employee|
+      employee_credentials = time_entries_per_employee[0]
+
+      Employee.find_or_create_by!(redmine_user_id: employee_credentials[:redmine_user_id]) do |employee|
+        employee.name = employee_credentials[:name]
+        employee.open_investment_time = 0
+      end
+    end
+  end
+
+  def add_time_entries_to_employees
+    @employees = OpenInvestmentTimeCalculator.new(@grouped_time_entries).add_time_entries_to_employees
+  end
+
+  def save_information_to_db
+    save_new_employees
+    save_current_time_to_db
+  end
+
+  def save_new_employees
+    Employee.transaction do
+      @employees.each(&:save!)
+    end
+  end
+
+  def save_current_time_to_db
+    SaveImportTime.new.save_current_time_to_db
+  end
+end
---
+++ spec/services/add_newest_time_entries_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe AddNewestTimeEntries, type: :service do
+  let(:all_time_entries) do
+    [{ 'user' => { 'name' => 'Max', 'id' => 1 }, 'project' => { 'id' => 1 }, 'hours' => 10 },
+     { 'user' => { 'name' => 'Max', 'id' => 1 }, 'project' => { 'id' => 140 }, 'hours' => 1 },
+     { 'user' => { 'name' => 'Leo', 'id' => 2 }, 'project' => { 'id' => 129 }, 'hours' => 2 },
+     { 'user' => { 'name' => 'Duda', 'id' => 3 }, 'project' => { 'id' => 140 }, 'hours' => 3 }]
+  end
+
+  context 'if database is empty' do
+    describe 'add to db' do
+      it 'adds the new entries to the db' do
+        described_class.new(all_time_entries).add_to_db
+        expect(Employee.exists?(redmine_user_id: 1)).to be true
+        expect(Employee.exists?(redmine_user_id: 2)).to be true
+        expect(Employee.exists?(redmine_user_id: 3)).to be true
+      end
+
+      it 'adds the new investment time to the user' do
+        described_class.new(all_time_entries).add_to_db
+        expect(Employee.find_by(redmine_user_id: 1).open_investment_time).to be(1.0)
+        expect(Employee.find_by(redmine_user_id: 2).open_investment_time).to be(-2.0)
+        expect(Employee.find_by(redmine_user_id: 3).open_investment_time).to be(-3.0)
+      end
+
+      it 'adds the current time to the db' do
+        described_class.new(all_time_entries).add_to_db
+        expect(Import.all.size).to be(1)
+      end
+    end
+  end
+
+  context 'if database is not empty' do
+    describe 'add to db' do
+      before(:each) do
+        Employee.create(redmine_user_id: 1, name: 'Max', open_investment_time: 2)
+        Employee.create(redmine_user_id: 2, name: 'Leo', open_investment_time: 79)
+        Employee.create(redmine_user_id: 3, name: 'Duda', open_investment_time: 79)
+      end
+
+      it 'adds the new entries to the db' do
+        described_class.new(all_time_entries).add_to_db
+        expect(Employee.exists?(redmine_user_id: 1)).to be true
+        expect(Employee.exists?(redmine_user_id: 2)).to be true
+        expect(Employee.exists?(redmine_user_id: 3)).to be true
+      end
+    end
+  end
+end
