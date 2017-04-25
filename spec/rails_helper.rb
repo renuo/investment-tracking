4854dd105477852d8366eacb6a1ec8c52deccf49
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-04-25 11:35:13 +0200

    Add database cleaner again because tests are failing on github

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,18 @@ Capybara.javascript_driver = :webkit
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
   config.before(:each) do
     DatabaseCleaner.start
   end
