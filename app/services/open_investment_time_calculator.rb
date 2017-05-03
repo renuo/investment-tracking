3aa4f13f02f14ebc14758020bfa9037f233c7ce2
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-05-03 16:54:05 +0200

    Refactor open investment time calculator

--- app/services/open_investment_time_calculator.rb
+++ app/services/open_investment_time_calculator.rb
@@ -1,6 +1,5 @@
 class OpenInvestmentTimeCalculator
-  def initialize(time_entries)
-    @time_entries = time_entries
+  def initialize
     @updated_times_of_employees = nil
 
     @employee = { redmine_id: nil, entries: nil, investment_time: nil, employee_from_db: nil }
@@ -9,53 +8,32 @@ class OpenInvestmentTimeCalculator
     @limit_of_investment_time = InvestmentTracking::Application::MAXIMUM_OF_INVESTMENT_TIME
   end
 
-  def sum_entries_rely_on_project_id
-    @updated_times_of_employees = @time_entries.map do |time_entries_per_employee|
-      extract_information(time_entries_per_employee)
-      add_entries_to_investment_time
-      set_new_investment_time
-      @employee[:employee_from_db]
+  def sum_entries_rely_on_project_id(grouped_time_entries)
+    grouped_time_entries.map do |user, entries|
+      user_entries = entries
+      employee = Employee.find_by(redmine_user_id: user[:redmine_user_id])
+
+      calculate_open_investment_time(employee, user_entries)
     end
   end
 
   private
 
-  def extract_information(employee_entries)
-    employee_from_time_entries = employee_entries[0]
-    @employee[:redmine_id] = employee_from_time_entries[:redmine_user_id]
-    @employee[:entries] = employee_entries[1]
-
-    employee_from_db = Employee.find_by(redmine_user_id: @employee[:redmine_id])
-    @employee[:employee_from_db] = employee_from_db
-    @employee[:investment_time] = employee_from_db.open_investment_time
-  end
-
-  def add_entries_to_investment_time
-    @employee[:entries].each do |time_entry|
-      if entry_is_investment_time(time_entry)
-        subtract_investment_time(time_entry['hours'])
+  def calculate_open_investment_time(employee, time_entries)
+    time_entries.each do |time_entry|
+      open_investment_time = employee.open_investment_time
+      if investment_projects.ids.include? time_entry['project']['id']
+        open_investment_time -= time_entry['hours']
       else
-        add_investment_time(time_entry['hours'])
-        if @employee[:investment_time] > @limit_of_investment_time
-          @employee[:investment_time] = @limit_of_investment_time
-        end
+        open_investment_time += time_entry['hours'] / @ratio_of_investment_time
       end
+
+      employee.open_investment_time = [open_investment_time, @limit_of_investment_time].min
     end
+    employee
   end
 
-  def entry_is_investment_time(time_entry)
-    RedmineInvestmentProjects.new.ids.include? time_entry['project']['id']
-  end
-
-  def subtract_investment_time(investment_hours)
-    @employee[:investment_time] -= investment_hours
-  end
-
-  def add_investment_time(worked_hours)
-    @employee[:investment_time] += worked_hours / @ratio_of_investment_time
-  end
-
-  def set_new_investment_time
-    @employee[:employee_from_db].open_investment_time = @employee[:investment_time]
+  def investment_projects
+    @redmine_investment_project ||= RedmineInvestmentProjects.new
   end
 end
--- spec/services/open_investment_time_calculator_spec.rb
+++ spec/services/open_investment_time_calculator_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe OpenInvestmentTimeCalculator, type: :service do
       end
 
       it 'adds the time entries to the existing investment time of the db' do
-        updated_employees = described_class.new(time_entries).sum_entries_rely_on_project_id
+        updated_employees = described_class.new.sum_entries_rely_on_project_id(time_entries)
         expect(updated_employees[0].open_investment_time).to be(4.5)
         expect(updated_employees[1].open_investment_time).to be(75.0)
       end
@@ -33,7 +33,7 @@ RSpec.describe OpenInvestmentTimeCalculator, type: :service do
       end
 
       it 'adds the time entries to the existing investment time of the db' do
-        updated_employees = described_class.new(time_entries).sum_entries_rely_on_project_id
+        updated_employees = described_class.new.sum_entries_rely_on_project_id(time_entries)
         expect(updated_employees[0].open_investment_time).to be(4.5)
         expect(updated_employees[1].open_investment_time).to be(80.0)
       end
