2110875e72b7083627f10984eca8fdff6bc7633c
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-05-03 16:59:00 +0200

    Rename parameters from csv_parser

--- app/services/csv_parser.rb
+++ app/services/csv_parser.rb
@@ -3,8 +3,8 @@ class CsvParser
 
   PARSER_ERROR_MESSAGE = 'Malformed CSV, please use comma delimiters (Redmine language setting?)'.freeze
 
-  def initialize(request)
-    @csv_body = request.body
+  def initialize(body)
+    @response_body = body
     @separated_csv_entries = nil
     @keys = nil
   end
@@ -13,13 +13,12 @@ class CsvParser
     separate_csv
     extract_keys
     convert_to_hashes
-    encode_user_name
   end
 
   private
 
   def separate_csv
-    @separated_csv_entries = CSV.parse(@csv_body, col_sep: ',')
+    @separated_csv_entries = CSV.parse(@response_body, col_sep: ',')
   rescue CSV::MalformedCSVError
     raise PARSER_ERROR_MESSAGE
   end
@@ -30,11 +29,5 @@ class CsvParser
 
   def convert_to_hashes
     @separated_csv_entries = @separated_csv_entries.map { |value| @keys.zip value }.map(&:to_h)
-  end
-
-  def encode_user_name
-    @separated_csv_entries.each do |entry|
-      entry['User'] = entry['User'].force_encoding('ISO-8859-1').encode('UTF-8')
-    end
   end
 end
