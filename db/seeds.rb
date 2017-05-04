331d930ec0f7bf596093be006f4728be65856fad
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-05-04 17:42:40 +0200

    Adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-RedmineImport.create(created_at: 'Mon, 30 Jan 2017 00:00:00 CEST +02:00')
+RedmineImport.create(created_at: 'Mon, 30 Jan 2017 02:00:00 CEST +02:00')
