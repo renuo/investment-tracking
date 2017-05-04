8ed0c703c7af0ab05dd50964ef0c5fa8eb533063
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-05-04 17:42:17 +0200

    Add new investment project

--- spec/services/redmine_investment_projects_spec.rb
+++ spec/services/redmine_investment_projects_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RedmineInvestmentProjects do
   subject { described_class.new }
   describe '#ids' do
     it 'gives back an array with all investment project ids' do
-      expect(subject.ids).to eq([138, 129, 141, 130, 139, 140])
+      expect(subject.ids).to eq([138, 152, 129, 141, 130, 139, 140])
     end
   end
 end
