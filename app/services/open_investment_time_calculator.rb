91dcb7bb79034104990e3a4f920f3fa4aa974d8d
Alicia Rueegg <Alicia.rueegg@renuo.ch>
2017-06-28 16:29:20 +0200

    Delete unnecessary chances in class

--- app/services/open_investment_time_calculator.rb
+++ app/services/open_investment_time_calculator.rb
@@ -19,10 +19,10 @@ class OpenInvestmentTimeCalculator
   def calculate_open_investment_time(employee, time_entries)
     time_entries.each do |time_entry|
       open_investment_time = employee.open_investment_time
-      if investment_project(time_entry)
+      if investment_projects.ids.include? time_entry['project']['id']
         open_investment_time -= time_entry['hours']
       else
-        open_investment_time += new_investment_time(time_entry)
+        open_investment_time += time_entry['hours'] / @ratio_of_investment_time
       end
 
       employee.open_investment_time = [open_investment_time, @limit_of_investment_time].min
@@ -30,25 +30,7 @@ class OpenInvestmentTimeCalculator
     employee
   end
 
-  def new_investment_time(time_entry)
-    if !time_entry.key?('issue')
-      time_entry['hours'] / @ratio_of_investment_time
-    elsif !(excluded_tickets.ticket_nrs.include? time_entry['issue']['id'])
-      time_entry['hours'] / @ratio_of_investment_time
-    else
-      0
-    end
-  end
-
-  def investment_project(time_entry)
-    investment_projects.ids.include? time_entry['project']['id']
-  end
-
   def investment_projects
     @redmine_investment_project ||= RedmineInvestmentProjects.new
-  end
-
-  def excluded_tickets
-    @redmine_excluded_tickets ||= RedmineExcludedTickets.new
   end
 end
